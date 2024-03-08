5a2664c75895e8bb284cc5cfc004240670fd4e16
Chen Zhao <43800081+zxchen98@users.noreply.github.com>
2024-03-07 19:53:27 -0800

    zxc

---
+++ ApplicationCore/Contracts/Repository/IBaseRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApplicationCore.Contracts.Repository
+{
+    public interface IBaseRepository<T> where T : class
+    {
+        int Insert(T entity);
+        int Update(T entity);
+        int Delete(int id);
+        IEnumerable<T> GetAll();
+        T GetById(int id);
+    }
+}
---
+++ Infrastructure/Repository/BaseRepository.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ApplicationCore.Contracts.Repository;
+using Infrastructure.Data;
+
+namespace Infrastructure.Repository
+{
+    internal class BaseRepository<T>:IBaseRepository<T> where T:class
+    {
+        protected readonly ProductDBContext _context;
+        public BaseRepository(ProductDBContext tb) { 
+            _context = tb;
+        }
+
+        public int Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<T> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public T GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int Insert(T entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int Update(T entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
