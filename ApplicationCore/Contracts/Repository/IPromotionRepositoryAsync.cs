79dce7f02de89fd3be02ea08bc4b10f8416b8298
Chen Zhao <43800081+zxchen98@users.noreply.github.com>
2024-03-15 13:37:33 -0700

    entities

--- ApplicationCore/Contracts/Repository/IPromotionRepositoryAsync.cs
+++ ApplicationCore/Contracts/Repository/IPromotionRepositoryAsync.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace ApplicationCore.Contracts.Repository
 {
-    internal interface IPromotionRepositoryAsync
+    public interface IPromotionRepositoryAsync
     {
     }
 }
