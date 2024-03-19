eda34840a5a33cbfdd6730047230bc77cbb5e308
Chen Zhao <43800081+zxchen98@users.noreply.github.com>
2024-03-19 12:12:47 -0700

    mapper

--- EShopAPI/Program.cs
+++ EShopAPI/Program.cs
@@ -13,7 +13,7 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
+builder.Services.AddAutoMapper(typeof(Program));
 builder.Services.AddDbContext<TrainingDbContext>(option => {
 
     option.UseSqlServer(builder.Configuration.GetConnectionString("TrainingDB"));
--- Infrastructure/Data/Mapping.cs
+++ Infrastructure/Data/Mapping.cs
@@ -6,10 +6,18 @@ using System.Threading.Tasks;
 using AutoMapper;
 using ApplicationCore.Entities;
 using ApplicationCore.Models;
+using ApplicationCore.Models.Response;
+using ApplicationCore.Models.Request;
 
 namespace Infrastructure.Data
 {
-    public class Mapping
+    public class Mapping : Profile
     {
+        public MappingProfile()
+        {
+
+            CreateMap<Product, ProductResponse>();
+            CreateMap<ProductRequest, Product>();
+        }
     }
 }
